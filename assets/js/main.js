213321cd82eb6f0017bdc83b9e46dfe919f2c7be
Arun chandra roy <48654733+codersurjo@users.noreply.github.com>
2022-09-27 16:48:27 +0600

    Contact form complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,60 @@
 $(document).ready(function(){
     $('select').niceSelect();
+
+    // Owl Carousel
+    var hero_slider = $(".hero_slider");
+    hero_slider.owlCarousel({
+        items: 1,
+        margin: 0,
+        loop: true,
+        nav: true,
+        dots:true,
+    });
+
+    // Forbes Slider
+    var fore_slider = $(".fore_slider");
+    fore_slider.owlCarousel({
+        items: 3,
+        margin: 15,
+        loop: true,
+        nav: true,
+        dots:true,
+        responsive: {
+            0:{
+              items: 1
+            },
+            480:{
+              items: 2
+            },
+            769:{
+              items: 3
+            }
+        }
+
+    });
+
+     // partner Slider
+     var partnar_slider = $(".partnar_slider");
+     partnar_slider.owlCarousel({
+         items: 8,
+         margin: 10,
+         loop: true,
+         nav: true,
+         dots:true,
+         responsive: {
+             0:{
+               items: 3
+             },
+             480:{
+               items: 5
+             },
+             769:{
+               items: 7
+             },
+             992:{
+                items:8
+             }
+         }
+ 
+     });
 })
